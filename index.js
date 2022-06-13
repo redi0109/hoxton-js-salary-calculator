9fc30e3a1d7c13b0413cd721d117cb6d3f055735
Demir Boraj <demirboraj97@gmail.com>
2022-06-13 17:37:02 +0200

    Net salary for 500 euro

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const taxableAmount = grossSalary - pensionContribution;
 const taxUnder80Euro = 0;
 const taxunder250Euro = (250 - 80) *0.04;
 const taxunder450Euro = (450 - 250) *0.08 + taxunder250Euro;
-const taxover450Euro = ( taxableAmount - 450) *0.1 + taxunder450Euro;
+const taxover450Euro = (grossSalary - 450) *0.1 + taxunder450Euro;
 const netSalary = taxableAmount - taxover450Euro;
-console.log(`Gross salary: ${netSalary}`);
+console.log(`Net salary: ${netSalary}`);
